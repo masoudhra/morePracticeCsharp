dde588cc73c378be82e798d917928aa52b95c33e
Masoud Ramezani <masoudhra@gmail.com>
2023-08-01 15:21:12 +1200

    Practice C#

---
+++ pr/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] evenNumber = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                evenNumber[i] = i * 2;
+            }
+
+            for (int i = evenNumber.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(evenNumber[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
